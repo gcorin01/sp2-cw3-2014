d2c7b513b671de03c7d4c15d0dfbc306bea76237
gcorin01 <=>
2014-12-13 17:38:16 +0000

    Added abstract RideStatus class to handle the fact that a Customer can be waiting for a lift or be in it.

---
+++ src/uk/ac/bbk/sp2/cw3/simple_elevator_simulator/MockRideStatus.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package uk.ac.bbk.sp2.cw3.simple_elevator_simulator;
+
+/**
+ * @author Gabriel
+ *
+ */
+public class MockRideStatus extends RideStatus {
+    
+    // Created to test the RideStatus abstract class
+    
+    
+
+}
---
+++ src/uk/ac/bbk/sp2/cw3/simple_elevator_simulator/RideStatusTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package uk.ac.bbk.sp2.cw3.simple_elevator_simulator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Gabriel
+ *
+ */
+public class RideStatusTest {
+    
+    RideStatus rideStatus;
+    @Before
+    public void setUp() {
+	rideStatus = new MockRideStatus();
+    }
+
+/*    @Test
+    public void testRideStausClassExistance() {
+	@SuppressWarnings("unused")
+	MockRideStatus rideStatus = new MockRideStatus();
+    }
+
+    @Test
+    public void test() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void test() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void test() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void test() {
+	fail("Not yet implemented");
+    }*/
+}
